b1e9c63431ef30331d60ec5db67cecf2365b28a0
Spegatti <145265002+Spegatti@users.noreply.github.com>
2023-09-19 00:28:49 -0700

    Update EventListener.php

--- src/Terpz710/AnimationJoin/EventListener.php
+++ src/Terpz710/AnimationJoin/EventListener.php
@@ -2,15 +2,88 @@
 
 namespace Terpz710\AnimationJoin;
 
-use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\plugin\Plugin;
+use pocketmine\network\mcpe\protocol\LevelEventPacket;
+use pocketmine\network\mcpe\protocol\types\LevelEvent;
 use pocketmine\utils\Config;
-use Terpz710\AnimationJoin\EventListener;
+use pocketmine\Player;
 
-class Main extends PluginBase {
+class EventListener implements Listener {
 
-    public function onEnable(): void {
-        $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this, $config), $this);
-        $this->saveDefaultConfig();
+    private $plugin;
+    private $config;
+
+    public function __construct(Plugin $plugin, Config $config) {
+        $this->plugin = $plugin;
+        $this->config = $config;
+    }
+
+    public function onPlayerJoin(PlayerJoinEvent $event): void {
+        $player = $event->getPlayer();
+
+        $enableMOTD = $this->config->getNested("Enable.MOTD", true);
+        $enableMessages = $this->config->getNested("Enable.Messages", true);
+        $enableTitle = $this->config->getNested("Enable.Title", true);
+        $enableJoinAnimation = $this->config->getNested("Enable.Join-Animation", true);
+
+        if ($enableMOTD && $this->config->get("MOTD") !== null) {
+            $motd = $this->config->get("MOTD");
+            $player->sendMessage($motd);
+        }
+
+        $customJoinConfig = $this->config->get("Messages.Join-Message");
+        $customLeaveConfig = $this->config->get("Messages.Leave-Message");
+
+        if ($enableMessages) {
+            $joinMessage = str_replace("%player%", $player->getName(), $customJoinConfig);
+            $player->sendMessage($joinMessage);
+        }
+
+        $titleConfig = $this->config->get("Title");
+        if ($enableTitle) {
+            $player->addTitle(
+                $titleConfig["Title-Text"],
+                $titleConfig["Subtitle-Text"],
+                $titleConfig["Fade-In"],
+                $titleConfig["Stay"],
+                $titleConfig["Fade-Out"]
+            );
+        }
+
+        $joinAnimationConfig = $this->config->get("Join-Animation");
+        if ($enableJoinAnimation) {
+            $chosenAnimation = $joinAnimationConfig["Animation"];
+            if ($chosenAnimation === "totem") {
+                $animation = new TotemUseAnimation($player);
+                $animations = $animation->encode();
+
+                foreach ($animations as $packet) {
+                    $player->getNetworkSession()->sendDataPacket($packet);
+                }
+            } elseif ($chosenAnimation === "guardian") {
+                $player->getNetworkSession()->sendDataPacket(
+                    LevelEventPacket::create(
+                        eventId: LevelEvent::GUARDIAN_CURSE,
+                        eventData: 0,
+                        position: $player->getPosition()
+                    )
+                );
+            }
+        }
+    }
+
+    public function onPlayerQuit(PlayerQuitEvent $event): void {
+        $player = $event->getPlayer();
+
+        $enableMessages = $this->config->getNested("Enable.Messages", true);
+        $customLeaveConfig = $this->config->get("Messages.Leave-Message");
+
+        if ($enableMessages) {
+            $leaveMessage = str_replace("%player%", $player->getName(), $customLeaveConfig);
+            $event->setQuitMessage($leaveMessage);
+        }
     }
 }
