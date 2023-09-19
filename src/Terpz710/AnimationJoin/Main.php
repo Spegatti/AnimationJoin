77d1392cab85128f11176d89901a51f5e5855505
Spegatti <145265002+Spegatti@users.noreply.github.com>
2023-09-19 00:29:26 -0700

    Update Main.php

--- src/Terpz710/AnimationJoin/Main.php
+++ src/Terpz710/AnimationJoin/Main.php
@@ -3,12 +3,13 @@
 namespace Terpz710\AnimationJoin;
 
 use pocketmine\plugin\PluginBase;
-use pocketminr\utils\Config;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase {
 
     public function onEnable(): void {
-        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+        $config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this, $config), $this);
         $this->saveDefaultConfig();
     }
 }
